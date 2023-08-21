61c0eca10dbe686b12a5bd8072f3d97667e9e74c
lorranenobre <lo.nobrem@gmail.com>
2023-08-21 15:38:02 -0300

    Fluxo de telas com rotas e tratamento de URL

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:go_router/go_router.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setUrlStrategy(PathUrlStrategy());
   runApp(const MainApp());
 }
 
@@ -8,13 +12,94 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
+  Widget build(BuildContext context){
+    final router = GoRouter(
+        initialLocation: '/',
+        routes: [
+          GoRoute(
+            path: '/',
+            pageBuilder: (context, state) => const MaterialPage(
+              child: TelaInicial(),
+            ),
+          ),
+          GoRoute(
+            path: '/telaA',
+            pageBuilder: (context, state) => const MaterialPage(
+              child: TelaA(),
+            ),
+          ),
+          GoRoute(
+            path: '/telaA/telaB',
+            pageBuilder: (context, state) => const MaterialPage(
+              child: TelaB(),
+            ),
+          ),
+        ]
+    );
+    return MaterialApp.router(
+      theme: ThemeData.dark(),
+      routerDelegate: router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
+    );
+  }
+}
+
+class TelaInicial extends StatelessWidget {
+  const TelaInicial({
+    super.key,
+  });
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Tela inicial'),
       ),
+      body: Center(
+        child: TextButton(
+            onPressed: () => GoRouter.of(context).go('/telaA'),
+            child: Text('VAI PRA TELA A!')),
+      ),
+    );
+  }
+}
+
+class User extends StatelessWidget{
+  const User({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tela A'),
+      ),
+      body: Column(
+        children: [
+          const Center(
+              child: Text('Estamos na Tela A.')),
+          TextButton(
+              onPressed: () => GoRouter.of(context).go('/users/details'),
+              child: const Text('VAI PARA A TELA ')),
+        ],
+      ),
+    );
+  }
+}
+
+class Details extends StatelessWidget{
+  const Details({super.key, required this.id});
+
+  final String id;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tela detalhes'),
+      ),
+      body: const Center(
+          child: Text(id)),
+    ),
     );
   }
 }
