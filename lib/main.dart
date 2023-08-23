77e2a207c46df2d6da9403420a49203a7287fb9c
LoNobre <51967802+LoNobre@users.noreply.github.com>
2023-08-23 11:33:07 -0300

    Update main.dart
    Separação de classes criadas na main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:go_router/go_router.dart';
 
+import 'pages/about.dart';
+import 'pages/contacts.dart';
+import 'pages/description.dart';
+import 'pages/home.dart';
+import 'pages/informationsCollege.dart';
+import 'pages/projects.dart';
+
 void main() {
   setUrlStrategy(PathUrlStrategy());
   runApp(const MainApp());
@@ -18,13 +25,7 @@ class MainApp extends StatelessWidget {
           GoRoute(
             path: '/',
             pageBuilder: (context, state) => const MaterialPage(
-              child: TelaInicial(),
-            ),
-          ),
-          GoRoute(
-            path: '/users',
-            pageBuilder: (context, state) => const MaterialPage(
-              child: User(),
+              child: Home(),
             ),
           ),
           GoRoute(
@@ -57,17 +58,6 @@ class MainApp extends StatelessWidget {
               child: About(),
             ),
           ),
-          GoRoute(
-            path: '/users/details/:id',
-            pageBuilder: (context, state) {
-              String id = state.pathParameters['id']!;
-              return MaterialPage(
-                child: Details(
-                  id: id,
-                ),
-              );
-            },
-          )
         ],
     );
 
@@ -76,168 +66,6 @@ class MainApp extends StatelessWidget {
       routerDelegate: router.routerDelegate,
       routeInformationParser: router.routeInformationParser,
       routeInformationProvider: router.routeInformationProvider,
-    );
-  }
-}
-
-class About extends StatelessWidget{
-  const About({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('TELA DE SOBRE'),
-      ),
-      body: Column(
-        children: [
-          const Center(
-              child: Text('Estamos na tela de SOBRE')),
-          TextButton(onPressed: () => GoRouter.of(context).go('/about'),
-              child: const Text('VAI PARA A TELA DE PROJETOS')),
-        ],
-      ),
-    );
-  }
-}
-
-class InformationsCollege extends StatelessWidget{
-  const InformationsCollege({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('TELA DE PROJETOS'),
-      ),
-      body: Column(
-        children: [
-          const Center(
-              child: Text('Estamos na tela de informações acadêmicas')),
-          TextButton(onPressed: () => GoRouter.of(context).go('/informationsCollege'),
-              child: const Text('VAI PARA A TELA DE INFORMAÇÕES')),
-        ],
-      ),
-    );
-  }
-}
-
-class Description extends StatelessWidget{
-  const Description({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('TELA DE DESCRIÇÕES'),
-      ),
-      body: Column(
-        children: [
-          const Center(
-              child: Text('Estamos na tela de Descrições')),
-          TextButton(onPressed: () => GoRouter.of(context).go('/description'),
-              child: const Text('VAI PARA A TELA DE DESCRIÇÕES')),
-        ],
-      ),
-    );
-  }
-}
-
-class Contacts extends StatelessWidget{
-  const Contacts({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('TELA DE CONTATOS'),
-      ),
-      body: Column(
-        children: [
-          const Center(
-              child: Text('Estamos na tela de CONTATOS')),
-          TextButton(onPressed: () => GoRouter.of(context).go('/contacts'),
-              child: const Text('VAI PARA A TELA DE CONTATOS')),
-        ],
-      ),
-    );
-  }
-}
-
-class Projects extends StatelessWidget{
-  const Projects({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('TELA DE PROJETOS'),
-      ),
-      body: Column(
-        children: [
-          const Center(
-            child: Text('Estamos na tela de projetos realizados')),
-            TextButton(onPressed: () => GoRouter.of(context).go('/projects'),
-                child: const Text('VAI PARA A TELA DE PROJETOS')),
-        ],
-      ),
-    );
-  }
-}
-
-class TelaInicial extends StatelessWidget {
-  const TelaInicial({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Tela inicial'),
-      ),
-      body: Center(
-        child: TextButton(
-            onPressed: () => GoRouter.of(context).go('/users'),
-            child: Text('VAI PRA TELA DE USUÁRIOS')),
-      ),
-    );
-  }
-}
-
-class User extends StatelessWidget{
-  const User({super.key});
-
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(' TELA DE USUÁRIOS'),
-      ),
-      body: Column(
-        children: [
-          const Center(
-              child: Text('Estamos na Tela DE USUÁRIOS.')),
-          TextButton(
-              onPressed: () => GoRouter.of(context).go('/users/details/id'),
-              child: const Text('VAI PARA A TELA DE DETALHES')),
-        ],
-      ),
-    );
-  }
-}
-
-class Details extends StatelessWidget{
-  const Details({super.key, required this.id});
-
-  final String id;
-
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DETALHES'),
-      ),
-      body: Center(child: Text(id)),
     );
   }
 }
