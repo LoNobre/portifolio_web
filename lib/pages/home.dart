37593ae2c61758bb91ad920bb6e7c5ecd3e51745
lorranenobre <lo.nobrem@gmail.com>
2023-08-30 18:48:53 -0300

    Ajuste do nome, imagem e descricao profissional

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -42,7 +42,7 @@ class _HomeState extends State<Home> {
                       child: Text("Lorrane Nobre",
                         style: TextStyle(
                             fontSize: 36,
-                            fontWeight: FontWeight.w700,
+                            fontWeight: FontWeight.bold,
                             color: Colors.white70),
                       ),
                     ),
@@ -59,7 +59,21 @@ class _HomeState extends State<Home> {
                 ],
                 ),
               ),
-              Expanded(flex: 7, child: Container(color: Colors.orange)),
+              Expanded(flex: 7,
+                child: Center(
+                  child: SizedBox(
+                    height: 40,
+                    width: 80,
+                    child: Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                    textAlign: TextAlign.justify,
+                      style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 36
+                      ),
+                    ),
+                  ),
+                  ),
+              ),
             ],
           ),
         ),
