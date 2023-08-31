0e8976500d98cca95cf125394baca3b17fc05208
lorranenobre <lo.nobrem@gmail.com>
2023-08-31 09:42:34 -0300

    Ajuste de proporcao do texto na tela Home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -66,19 +66,24 @@ class _HomeState extends State<Home> {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Center(
-                      child: SizedBox(
-                        height: size.height * 0.4,
-                        width: size.width * 0.45,
-                        child: Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-                          textAlign: TextAlign.justify,
-                            style: TextStyle(
-                              color: Colors.white70,
-                              fontSize: size.width * 0.02,
+                    if (MediaQuery.of(context).size.width < 1000)
+                      Expanded(flex: 3, child: Container()),
+                      Expanded(flex: 4, child: Container(
+                        child: Center(
+                          child: SizedBox(
+                            height: size.height * 0.4,
+                            width: size.width * 0.45,
+                            child: Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                color: Colors.white70,
+                                fontSize: size.width * 0.02,
+                              ),
                             ),
+                          ),
                         ),
                       ),
-                    ),
+                      ),
                   ],
                 ),
               ),
